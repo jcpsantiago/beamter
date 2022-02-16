856a37744ca7831149e732681ea570eb8ee0a2a3
João Santiago <santiago@billie.io>
2022-02-16 13:06:11 +0100

    all functions needed working

--- R/builder.R
+++ R/builder.R
@@ -1,4 +1,4 @@
-get_registry_features_needed <- function(feature_registry, features){
+get_registry_features_needed <- function(feature_registry, features) {
   features[features %in% names(feature_registry)]
 }
 
@@ -23,19 +23,78 @@ get_base_features_needed <- function(feature_registry, feature) {
     `c`(., deps_to_explore)
 }
 
-get_all_features_needed <- function(feature_registry, features){
+get_all_base_features_needed <- function(feature_registry, features) {
   get_registry_features_needed(feature_registry, features) %>%
     purrr::map(., ~ get_base_features_needed(feature_registry, .x)) %>%
     purrr::reduce(`c`) %>%
     unique(.)
 }
 
-build_recipe_call_factory <- function(features) {
+get_unneeded_recipe_vars <- function(unprepped_recipe_full, feature_and_deps_names_used, features) {
+  recipe_var_info <- unprepped_recipe_full$var_info
+
+  c(
+    recipe_var_info$variable[
+      recipe_var_info$role == "predictor" &
+        !(recipe_var_info$variable %in% features)
+    ],
+    # we want to discard unnecessary base features and only keep the final features
+    unique(
+      setdiff(
+        feature_and_deps_names_used,
+        intersect(features, feature_and_deps_names_used)
+      )
+    )
+  )
+}
+
+collect_features_and_deps <- function(feature_registry, features) {
+  registry_features_needed <- get_registry_features_needed(feature_registry, features)
+  base_features_needed <- get_all_base_features_needed(feature_registry, features)
+
+  feature_and_deps_names_used <- c(
+    base_features_needed, registry_features_needed
+  ) %>%
+    unique(.)
+}
+
+init_recipe <- function(df_for_recipe, recipe_roles) {
+  recipes::recipe(
+    df_for_recipe,
+    vars = names(df_for_recipe),
+    roles = recipe_roles
+  )
+}
+
+build_recipe_call_factory <- function(feature_registry) {
   function(recipe, feature) {
     rlang::call2(
-      features[[feature]]$step_fn,
+      feature_registry[[feature]]$step_fn,
       recipe,
-      !!!features[[feature]]$args
+      !!!feature_registry[[feature]]$args
     )
   }
+}
+
+assemble_recipe <- function(feature_registry, features, df_for_recipe, recipe_roles) {
+  unprepped_recipe_init <- init_recipe(df_for_recipe, recipe_roles)
+  features_and_deps_used <- collect_features_and_deps(feature_registry, features)
+  build_recipe <- build_recipe_call_factory(feature_registry)
+
+
+  unprepped_recipe_full <- features_and_deps_used %>%
+    purrr::reduce(
+      build_recipe,
+      .init = unprepped_recipe_init
+    ) %>%
+    eval(.)
+
+  unneeded_recipe_vars <- get_unneeded_recipe_vars(
+    unprepped_recipe_full, features_and_deps_used, features
+  )
+
+  unprepped_recipe_full %>%
+    recipes::step_rm(
+      dplyr::all_of(unneeded_recipe_vars)
+    )
 }
